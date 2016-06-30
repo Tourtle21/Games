070cddd3d671cf249972f46a7cd0c727fc961663
Tech Camp 108 <dixie108@thulium.cs.dixie.edu>
2016-06-30 16:59:00 -0600

    Started working on arrow game

--- Arrow/arrow.js
+++ Arrow/arrow.js
@@ -3,26 +3,35 @@ var y = 500;
 var xspeed = 0;
 var yspeed = 2;
 var power = 4;
+var run = false;
 setAnglePower(20, 50)
 xspeed *= power;
 yspeed *= power;
-setInterval(function() {
-	x += xspeed;
-	y -= yspeed;
-	yspeed -= 0.04
-	$("#arrow").css("left", x + "px")
-	$("#arrow").css("top", y + "px")
-});
+
 
 $(document).keydown(function(e) {
 	if (e.which == 32) {
-		console.log("space")
+		run = true;
 	}
+	if (e.which == 38) {
+		setAnglePower(power + 1)
+		console.log(power)
+	}
+	console.log(e.which)
 })
 
 function setAnglePower(spray, angle) {
 	power = spray * 0.5;
 	yspeed = angle/180;
 	xspeed = (1 - yspeed)
-	console.log(power)
 }
+
+setInterval(function() {
+	if (run) {
+		x += xspeed;
+		y -= yspeed;
+		yspeed -= 0.04
+		$("#arrow").css("left", x + "px")
+		$("#arrow").css("top", y + "px")
+	}
+});
